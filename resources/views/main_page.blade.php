5ef62bb724fc5711d7f618bbb4a17df5c5501789
madnessll <tolyann358@gmail.com>
2024-06-29 07:37:35 +0700

    теперь максимум 10 тем выводится у форума на главной стр

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -7,24 +7,18 @@
     @foreach ($forums as $forum)
         <div class="question">
             <a href="{{ route('forums.show', $forum->id) }}" class="question__forum">{{ $forum->name }}</a>
+            @php $count = 0; @endphp
             @foreach ($forum->topics as $topic)
+                @if ($count >= 10)
+                    @break
+                @endif
                 <div class="question__descr">
                     <div class="question__descr-left">
-                        <!-- <div class="question__descr-left-name">{{ $topic->title }}</div> -->
                         <a href="{{ route('topics.show', $topic->id) }}" class="question__descr-left-name">{{ $topic->title }}</a>
                         <div class="question__descr-left-descr">{{ $topic->content }}</div>
                     </div>
-                    <!-- <div class="question__descr-right">
-                        <div class="question__descr-right-img">
-                            <img src="{{ asset('images/ava.png') }}" alt="ava">
-                        </div>
-                        <div class="question__descr-right-descr">
-                            <div class="question__descr-right-last">Последний вопрос</div>
-                            <div class="question__descr-right-author">Автор</div>
-                            <div class="question__descr-right-time">Сегодня, 13:50</div>
-                        </div>
-                    </div> -->
                 </div>
+                @php $count++; @endphp
             @endforeach
         </div>
     @endforeach
